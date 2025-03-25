acba55ad305828198252d368254f3ed0b60a8ea6
npcdazai <prathammandavkar932@gmail.com>
2025-03-25 17:26:56 +0530

    Png

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,55 +1,54 @@
-import { Box, Button, HStack, Image, Text } from "@chakra-ui/react"
-import React from "react"
-import { NavLink } from "react-router-dom"
+import { Box, Button, HStack, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const navItems = [
-    {
-        id: 1,
-        name: "Home",
-        link: "/"
-    },
-    {
-        id: 2,
-        name: "Teachers",
-        link: "/teachers"
-    },
-    {
-        id: 3,
-        name: "Courses",
-        link: "/courses"
-    },
-    {
-        id: 3,
-        name: "About Us",
-        link: "/about-us"
-    }
-]
+    { id: 1, name: "Home", link: "/" },
+    { id: 2, name: "Teachers", link: "/teachers" },
+    { id: 3, name: "Courses", link: "/courses" },
+    { id: 4, name: "About Us", link: "/about-us" }
+];
 
 const Header = () => {
     return (
-        <HStack position="sticky" top={0} zIndex={100} bgColor="#F9FBFC" justifyContent="space-between" alignItems="center" px={8} >
-            <Image src="src/assets/icon.png" height="70px" width="auto" />
-            <HStack>
-                {navItems.map((val) => {
-                    return (
-                        <NavLink style={({ isActive }) => (
-                            {
+        <motion.div 
+            initial={{ opacity: 0, y: -20 }} 
+            animate={{ opacity: 1, y: 0 }} 
+            transition={{ duration: 0.5 }}
+        >
+            <HStack position="sticky" top={0} zIndex={100} bgColor="#F9FBFC" justifyContent="space-between" alignItems="center" px={8} >
+                <Image src="src/assets/icon.png" height="70px" width="auto" />
+                <HStack>
+                    {navItems.map((val, index) => (
+                        <motion.div 
+                            key={val.id} 
+                            initial={{ opacity: 0, x: -20 }} 
+                            animate={{ opacity: 1, x: 0 }} 
+                            transition={{ delay: index * 0.2, duration: 0.5 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <NavLink style={({ isActive }) => ({
                                 color: isActive ? "#1f2f5d" : "#808080",
                                 fontWeight: isActive ? 700 : 500
+                            })} to={val.link}>
+                                <Text mr={4} fontSize="sm">{val.name}</Text>
+                            </NavLink>
+                        </motion.div>
+                    ))}
+                </HStack>
 
-                            }
-                        )} to={val.link}>
-                            <Text mr={4} fontSize="sm" >{val.name}</Text>
-                        </NavLink>
-                    )
-                })}
+                <motion.div
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <Button bgColor={"#FACA10"} fontSize={"sm"}>
+                        Get in Touch
+                    </Button>
+                </motion.div>
             </HStack>
+        </motion.div>
+    );
+};
 
-            <Button bgColor={"#FACA10"} fontSize={"sm"}>
-                Get in Touch
-            </Button>
-        </HStack>
-    )
-}
-
-export default Header
+export default Header;
--- src/components/pagesComponent/WhyWeAre.jsx
+++ src/components/pagesComponent/WhyWeAre.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 
 const WhyWeAre = () => {
   return (
-    <VStack w="100%">
-            <Heading><Text color="#525FE1">Why we are</Text> best from others?</Heading>
+    <VStack w="100%" px={4} py={12}>
+          <VStack w="100%" gap={6} >
+          <Heading fontWeight={400} color="#1C1C1C" fontSize="5xl" ><Text as="span" color="#525FE1" fontWeight={700}>Why we are</Text> best from others?</Heading>
+          <Text w="80%" color="#333333" as="span" fontWeight={400} textAlign="center" >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.....</Text>
+          </VStack>
     </VStack>
   )
 }
